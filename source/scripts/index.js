0f3f7b83b3d8f95ec7b64b6dc75cd329d873a743
ElenaLisavina <lisavinaelena92@gmail.com>
2025-02-08 20:17:24 +0300

    Task9 Finish

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,15 @@
-/* в этот файл добавляет скрипты*/
-const button = document.querySelector('.main-nav__toggle');
-
+// source/scripts/index.js
+const button = document.querySelector('.main-header__toggle');
 const header = document.querySelector('.main-header');
-
 document.body.classList.remove('nojs');
 
-
 button.addEventListener('click', () => {
-  if(header.classList.contains('main-header--opened')){
+  if (header.classList.contains('main-header--opened')) {
     header.classList.add('main-header--closed');
     header.classList.remove('main-header--opened');
-    button.classList.add('main-nav__toggle--burger');
-    button.classList.remove('main-nav__toggle--open');
-  } else{
+  } else {
     header.classList.remove('main-header--closed');
     header.classList.add('main-header--opened');
-    button.classList.add('main-nav__toggle--open');
   }
 });
-
+//# sourceMappingURL=index.js.map
